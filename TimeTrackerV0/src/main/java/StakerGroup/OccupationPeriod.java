48b6aee8e968a649b2c646dfa2a04f1cf394f4cf
GrunMann <bulat.atnabaev@gmail.com>
2017-04-14 16:09:44 +0300

    JSR-310 use initiated

---
+++ TimeTrackerV0/src/main/java/StakerGroup/OccupationPeriod.java
@@ -0,0 +1,56 @@
+package StakerGroup;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class OccupationPeriod {
+
+    private final LocalTime occupationStartTime;
+    private LocalTime occupationEndTime;
+    private LocalTime lunchStartTime;
+    private LocalTime lunchEndTime;
+
+
+    OccupationPeriod(int occupationStartTimeH, int occupationStartTimeM) {
+        this.occupationStartTime = LocalTime.of(occupationStartTimeH, occupationStartTimeM);
+    }
+    public void endOccupation(int occupationEndTimeH, int occupationEndTimeM){
+        this.occupationEndTime = LocalTime.of(occupationEndTimeH,occupationEndTimeM);
+    }
+    public void startLunch(int lunchStartH,int lunchStartM){
+        this.lunchStartTime=LocalTime.of(lunchStartH,lunchStartM);
+    }
+    public void endLunch(int lunchFinishH,int lunchFinishM){
+        this.lunchEndTime= LocalTime.of(lunchFinishH,lunchFinishM);
+    }
+    public Duration occupationSummary(){
+        return  Duration
+                .between(occupationStartTime,occupationEndTime)
+                .minus(Duration.between(lunchStartTime,lunchEndTime)
+                );
+    }
+
+    public LocalTime getOccupationStartTime() {
+        return occupationStartTime;
+    }
+
+    public LocalTime getOccupationEndTime() {
+        return occupationEndTime;
+    }
+
+    public Duration getOccupationTime() {
+        return Duration.between(occupationStartTime, occupationEndTime);
+    }
+
+    public LocalTime getLunchStartTime() {
+        return lunchStartTime;
+    }
+
+    public LocalTime getLunchEndTime() {
+        return lunchEndTime;
+    }
+
+    public Duration getLunchTime() {
+        return Duration.between(lunchStartTime, lunchEndTime);
+    }
+}
--- TimeTrackerV0/src/test/java/StakerGroup/TestForTimeCounter.java
+++ TimeTrackerV0/src/test/java/StakerGroup/TestForTimeCounter.java
@@ -2,6 +2,8 @@ package StakerGroup;
 
 import org.junit.Test;
 
+import java.time.Duration;
+
 import static org.junit.Assert.assertTrue;
 
 public class TestForTimeCounter {
@@ -9,19 +11,14 @@ public class TestForTimeCounter {
 
     @Test
     public void dayDurationTest(){
-        int start=9;
-        int end=18;
-        OcupationPeriod day1 = new OcupationPeriod(9);
-        day1.endOcupation(end);
-        day1.startLunch(12);
-        day1.endLunch(13);
-        assertTrue(day1.occupationSummary()==end-start-(1));
+        int startHour=9, startMin=30;
+        int endHour=19, endMin=20;
+
+        OccupationPeriod day1 = new OccupationPeriod(startHour, startMin);
+        day1.endOccupation(endHour,endMin);
+        day1.startLunch(12, 0);
+        day1.endLunch(13,0);
+        Duration testDuration = day1.getOccupationTime().minus(day1.getLunchTime());
+        assertTrue(day1.occupationSummary().equals(testDuration));
     }
-   /* @Test
-    public void customLunchCountingTest(){
-        int lunchBegin=12;
-        int lunchEnd=13;
-        assertTrue(day1.lunchTime(lunchBegin,lunchEnd)==1);
-    }
-*/
 }
